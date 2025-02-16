f6a97b865f48dc6ffc1b33b97cc75c4b542ad91b
gabai62 <gabai62it@gmail.com>
2025-02-16 14:33:17 +0200

    make build

---
+++ index.f7b8199c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");[e.tHead,e.tBodies[0],e.tFoot].forEach(function(e){Array.from(e.rows).forEach(function(e){var o=e.cells[1].cloneNode(!0);e.insertBefore(o,e.cells[e.cells.length-1])})});
+//# sourceMappingURL=index.f7b8199c.js.map
